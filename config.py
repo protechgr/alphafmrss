85adca444a512b2c44914ea61fe898bda69ef1da
protechgr <info@protech.gr>
2024-12-04 20:26:53 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Set your GH Pages URL here e.g.:
-ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
+ghPagesURL = "https://protechgr.github.io/alphafmrss/"
 ghRawURL = "https://raw.githubusercontent.com/conoro/tiktok-rss-flat/main/"
 
 # ghPagesURL = "https://tiktokrss.conoroneill.com/"
